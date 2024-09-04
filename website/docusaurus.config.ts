e0b302df520a2a3185134ba7c25e8ac738edcfa1
Jason Dent <jason@streetsidesoftware.nl>
2024-09-04 08:55:13 +0200

    Add heart to copyright

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -154,7 +154,7 @@ const config: Config = {
                 //   ],
                 // },
             ],
-            copyright: `Copyright © ${new Date().getFullYear()} Street Side Software`,
+            copyright: `Copyright © ${new Date().getFullYear()} Street Side Software <img style="height:1em" src="/img/Logo Heart Street Side Software 16x16.png" />`,
         },
         prism: {
             theme: prismThemes.github,
